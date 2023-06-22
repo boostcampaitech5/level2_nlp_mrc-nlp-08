798de17ca3fbb8e3d5858d3d5a792fc6557fba32
Minjae Kang <kminjae618@gmail.com>
2023-06-22 01:56:13 +0000

    feat: enable passage retrieval during the validation stage
    #25

--- train.py
+++ train.py
@@ -1,18 +1,15 @@
 import os
 
 import pandas as pd
-
 from transformers import (AutoConfig, AutoModelForQuestionAnswering,
-                          AutoTokenizer, set_seed,
-                          TrainingArguments)
-
+                          AutoTokenizer, TrainingArguments, set_seed)
 from dataset import Dataset
 from QA_trainer import QuestionAnsweringTrainer
 from utils_taemin import (compute_metrics, data_collators,
-                          post_processing_function)
+                          post_processing_function, run_sparse_retrieval)
 
 
-def main(model_name, data_path):
+def main(model_name, data_path, eval_as_test=False):
 
     set_seed(42)
 
@@ -21,7 +18,19 @@ def main(model_name, data_path):
     model = AutoModelForQuestionAnswering.from_pretrained(model_name,config=config)
 
     train_data = Dataset(dataframe=pd.read_csv(os.path.join(data_path, "train_data.csv")), state="train", tokenizer=tokenizer)
-    val_data = Dataset(dataframe=pd.read_csv(os.path.join(data_path, "validation_data.csv")), state="valid", tokenizer=tokenizer)
+    eval_data_df = pd.read_csv(os.path.join(data_path, "validation_data.csv"))
+
+    if eval_as_test:
+        datasets = run_sparse_retrieval(
+            tokenize_fn=tokenizer.tokenize, data_path=data_path, datasets=eval_data_df.drop(["context"], axis=1), bm25="plus"
+        ) # bm25 => None(TF-IDF), Okapi, L, plus
+        extracted_context = datasets["validation"]["context"]
+        eval_examples = eval_data_df.assign(context=extracted_context)
+        eval_data = Dataset(dataframe=eval_examples, state="valid", tokenizer=tokenizer)
+
+    else:
+        eval_examples = eval_data_df
+        eval_data = Dataset(dataframe=eval_examples, state="valid", tokenizer=tokenizer)
 
     data_collator = data_collators(tokenizer)
 
@@ -42,8 +51,8 @@ def main(model_name, data_path):
         model=model,
         args=args,
         train_dataset=train_data,
-        eval_dataset=val_data,
-        eval_examples=pd.read_csv(os.path.join(data_path, "validation_data.csv")),
+        eval_dataset=eval_data,
+        eval_examples=eval_examples,
         tokenizer=tokenizer,
         data_collator=data_collator,
         post_process_function=post_processing_function,
@@ -54,4 +63,4 @@ def main(model_name, data_path):
 if __name__ == "__main__":
     model_name = 'klue/roberta-large'
     data_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), "csv_data")
-    main(model_name=model_name, data_path=data_path)
+    main(model_name=model_name, data_path=data_path, eval_as_test=True)
